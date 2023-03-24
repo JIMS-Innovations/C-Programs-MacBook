abab0fc97fa14c2f82765fde71927482513eb20b
Jesutofunmi <jimsufficiency@gmailcom>
2023-03-24 18:18:02 +0100

    Update employee.c file

--- employee.c
+++ employee.c
@@ -1,13 +1,13 @@
 /**
  * @file employee.c
  * @author Jesutofunmi Kupoluyi(jimsufficiency@gmail.com)
- * @brief  This is a program that creates and uses an employee 
+ * @brief  This is a program that creates and uses an employee
  *         oriented data structure.
  * @version 0.1
  * @date 2023-03-24
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 /* Including necessary libraries */
@@ -26,12 +26,11 @@ struct employee
 
 typedef struct employee employee_t;
 
-employee_t Employee_1 = {"John Samuel", {1, 1, 2020}, 500};
-employee_t Employee_2;
-
-
 int main(int argc, char const *argv[])
-{ 
+{
+    employee_t Employee_1 = {"John Samuel", {1, 1, 2020}, 500};
+    employee_t Employee_2;
+
     char buffer[64];
 
     printf("Please enter your name : ");
@@ -44,9 +43,8 @@ int main(int argc, char const *argv[])
     printf("Please enter your salary : ");
     scanf("%f", &Employee_2.salary);
 
-    printf("Employee 1:\n\n\tName: %s\n\tHire Date: %d/%d/%d\n\tSalary: %.2f\n\n",Employee_1.name, Employee_1.hireDate[0], Employee_1.hireDate[1], Employee_1.hireDate[2], Employee_1.salary);
-    printf("Employee 2:\n\n\tName: %s\tHire Date: %d/%d/%d\n\tSalary: %.2f\n\n",Employee_2.name, Employee_2.hireDate[0], Employee_2.hireDate[1], Employee_2.hireDate[2], Employee_2.salary);
-
+    printf("Employee 1:\n\n\tName: %s\n\tHire Date: %d/%d/%d\n\tSalary: %.2f\n\n", Employee_1.name, Employee_1.hireDate[0], Employee_1.hireDate[1], Employee_1.hireDate[2], Employee_1.salary);
+    printf("Employee 2:\n\n\tName: %s\tHire Date: %d/%d/%d\n\tSalary: %.2f\n\n", Employee_2.name, Employee_2.hireDate[0], Employee_2.hireDate[1], Employee_2.hireDate[2], Employee_2.salary);
 
     return 0;
 }
