1a7f15677deb7dcad40ff4e830560e26425fe9c8
Jesutofunmi <jimsufficiency@gmailcom>
2023-03-24 11:31:34 +0100

    Added new file: stringLen.c

---
+++ stringLen.c
@@ -0,0 +1,44 @@
+/**
+ * @file stringLen.c
+ * @author Jesutofunmi Kupoluyi(jimsufficiency@.com)
+ * @brief  This is a simple program that uses pointer 
+ *         arithmetic to calculate the length of a string.
+ * @version 0.1
+ * @date 2023-03-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/* Including necessary libraries */
+#include <stdio.h>
+#include <stddef.h>
+
+/* Function prototype */
+int my_strlen(char *);
+
+int main(int argc, char const *argv[])
+{
+    printf(" The length of string \"Hello\" is : %d \n", my_strlen("Hello"));
+    return 0;
+}
+
+/**
+ * @brief This is a function that calculates 
+ *  string length using pointer arithmetic
+ * 
+ * @param string 
+ */
+int my_strlen(char *string)
+{
+    int length = 0;
+    const char *const pFirst  = string;
+    
+    while (*(++string));
+    
+    const char *const pLast = string;
+
+    length = pLast - pFirst;
+
+    return length;
+}
